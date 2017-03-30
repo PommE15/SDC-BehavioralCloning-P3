62c18245a1bc5d6c3fd6d9493244aedc69d44f51
PommE15 <apple.chan-fardel@guardian.co.uk>
2017-03-31 00:05:03 +0100

    Add images and model to model.py

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 from keras.models import Sequential, Model
-from keras.layers import Cropping2D
+from keras.layers.core import Lambda, Dropout, Flatten, Dense
+from keras.layers.convolutional import Cropping2D, Conv2D
+from keras.layers.pooling import MaxPooling2D
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 #import sklearn
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,35 +14,13 @@ import os
 
 ### Load Data and Images
 samples = []
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
+csv_file = './data/driving_log.csv'
+with open(csv_file) as f:
+    reader = csv.reader(f)
+    # skip the headers
+    next(reader, None)  
     for line in reader:
         samples.append(line)
-        
-# Using Multipule Cameras
-with open(csv_file, 'r') as f:
-  reader = csv.reader(f)
-      for row in reader:
-          steering_center = float(row[3])
-
-          # create adjusted steering measurements for the side camera images
-          correction = 0.2 #TODO: tune
-          steering_left = steering_center + correction
-          steering_right = steering_center - correction
-
-          # read in images from center, left and right cameras
-          directory = ".data/"
-          img_center = process_image(np.asarray(Image.open(path + row[0])) 
-          img_left = process_image(np.asarray(Image.open(path + row[1])))
-          img_right = process_image(np.asarray(Image.open(path + row[2])))
-
-          # add images and angles to data set
-          car_images.extend(img_center, img_left, img_right)
-          steering_angles.extend(steering_center, steering_left, steering_right)
-  
-# Data Augmentation
-image_flipped = np.fliplr(image)
-measurement_flipped = -measurement
 
 
 ### Generator !? 
@@ -55,16 +36,32 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                # Using multipule cameras
+                
+                # TODO: tune
+                correction = 0.1
+                
+                path = './data/'
+                image_center = cv2.imread(path + batch_sample[0])
+                image_left   = cv2.imread(path + batch_sample[1].strip())
+                image_right  = cv2.imread(path + batch_sample[2].strip())
+                angle_center = float(batch_sample[3])
+                angle_left   = angle_center + correction
+                angle_right  = angle_center - correction
+                
+                # Data Augmentation
+                '''
+                #image_flipped = np.fliplr(image)
+                #measurement_flipped = -measurement
+                '''
+
+                images.extend([image_center, image_left, image_right])
+                angles.extend([angle_center, angle_left, angle_right])
 
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
+            yield shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
@@ -74,24 +71,54 @@ validation_generator = generator(validation_samples, batch_size=32)
 ### Training: NVIDIA Architecture
 # ref: images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf                               
 
-# Trimmed image format
-ch, row, col = 3, 80, 320  
-
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-# set up lambda layer
-model.add(Lambda(lambda x: x/255 - 0.5, input_shape=(ch, row, col), output_shape=(ch, row, col)))
 # set up cropping2D layer
-model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 20), (0, 0)), input_shape=(160, 320, 3)))
+
+# RGB -> YUV
+# cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 # ...
+
+# set up lambda layer
+model.add(Lambda(lambda x: x/255 - 0.5 ))
+
+# conv
+model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Conv2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Conv2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(64, 5, 5, subsample=(0, 0), activation='relu'))
+model.add(Conv2D(64, 5, 5, subsample=(0, 0), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+# fc
+model.add(Flatten())
+model.add(Dense(100, activation="relu"))
+model.add(Dense(50, activation="relu"))
+model.add(Dense(10, activation="relu"))
+
+# output
+model.add(Dense(1, activation='sigmoid'))
 
 
 ### Test and Evaluate !?
 model.compile(loss='mse', optimizer='adam')
-#model.fit_generator(train_generator, samples_per_epoch= /
-#    len(train_samples), validation_data=validation_generator, /
-#    nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(
+    train_generator, 
+    samples_per_epoch=len(train_samples), 
+    validation_data=validation_generator,
+    nb_val_samples=len(validation_samples), 
+    nb_epoch=1
+)
 
+# save model
+model.save('model.h5')
+
+'''
 history_object = model.fit_generator(
     train_generator, 
     samples_per_epoch=len(train_samples), 
@@ -112,3 +139,4 @@ plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
+'''
